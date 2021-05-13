d76c28e95a5a546a48a194e143e5f02ba05c39ac
Fadei007 <fkholyavo@xbsoftware.by>
2021-05-13 21:40:51 +0300

    feat: add minutes to the dhx_scale_hour cell

--- public/js/scheduler-init.js
+++ public/js/scheduler-init.js
@@ -1,12 +1,40 @@
 scheduler.config.xml_date="%Y-%m-%d %H:%i";
-			scheduler.init("scheduler_here", new Date(2018, 0, 20), "week");
+
+// Config date in headers of day and month views
+scheduler.templates.day_date = function(date){
+    const formatFunc = scheduler.date.date_to_str("%l, %d. %F %Y (week %W)");
+    return formatFunc(date);
+};
+
+scheduler.templates.week_date = function(start, end){
+	const parseStartDate = scheduler.date.date_to_str("%l, %d. %F");
+	const parseEndDate = scheduler.date.date_to_str("%l, %d. %F (week %W)");
+	const endDate = scheduler.date.add(end,-1,"day");
+    return parseStartDate(start) +" &ndash; " + parseEndDate(endDate);
+};
+
+// Add minutes to the dhx_scale_hour cell
+const hour = scheduler.date.date_to_str("%H:%i");
+const minutes = scheduler.date.date_to_str("%i");
 			
-			// load data from backend
-			scheduler.load("/events", "json");
+scheduler.config.hour_size_px = 77;
+scheduler.templates.hour_scale = function(date){
+	const step = 15;
+	let html = `<div class="dhx_scale_hour_hours">${hour(date)}</div>`;
+	for (let i = 0; i < 60/step; i++){
+		if(minutes(date) != "00")
+		html += `<div class="dhx_scale_hour_minutes">${minutes(date)}</div>`;
+		date = scheduler.date.add(date, step, "minute");
+	}
+	return html;
+}
 
-			// connect backend to scheduler
-			var dp = new dataProcessor("/events");
-			dp.init(scheduler);
+// Init sheduler
+scheduler.init("scheduler_here", new Date(), "week");
 
-			// set data exchange mode
-			dp.setTransactionMode("REST");
+scheduler.load("/events", "json");
+
+const dp = new dataProcessor("/events");
+dp.init(scheduler);
+
+dp.setTransactionMode("REST");
