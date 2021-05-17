878604ed43cb748d350b1937a48df680f9198d31
Fadei007 <fkholyavo@xbsoftware.by>
2021-05-17 23:09:06 +0300

    refact: move server side scripts to separate folder

--- server/server.js
+++ server/server.js
@@ -5,7 +5,8 @@ const app = express();
 const port = 3000;
  
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/../public/"));
+console.log(__dirname)
 
 const mysqlConfig = {
     "connectionLimit": 10,
